96046f75e3841fd5da32d4dc8710661cabe1065f
Alton Dupre <altondupre@eatel.net>
2024-09-16 20:09:25 -0500

    added main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// print("Hi Hi Hello Hello Hi Hi Hello O(∩_∩)O"
+
+#include <iostream>
+#include <stdio.h>
+#include "game.cpp"
+#include "win.cpp"
+
+int main(int argc, char const *argv[])
+{
+    printf("welcome to the machine\n");
+    printf("lets play a silly game!\n"); 
+
+    game();
+    you_win();
+
+
+    return 0;
+}
